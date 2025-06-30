1dbc70c25a841e4de230365b053c21fea371a30e
Yash Kapoor <yash.kpr2708@gmail.com>
2025-06-30 10:35:58 +0530

    Added create booking repository code

---
+++ src/utils/errors/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    ValidationError: require("sequelize"),
+    AppError: require('./app-error'),
+    ServiceError: require('./service-error')
+};
--- src/utils/errors/validation-error.js
+++ src/utils/errors/validation-error.js
@@ -8,7 +8,7 @@ class ValidationError extends Error {
         });
         this.name = 'ValidationError';
         this.message = 'Not able to validate the data sent in the request';
-        this.explanation = explanation,
+        this.explanation = explanation;
         this.statusCodes = StatusCodes.BAD_REQUEST;
     }
 }
